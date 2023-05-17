1b9c9989d138d77b92444b2e9772402b56b7c052
Kair_Wne <132913328+0KairWne0@users.noreply.github.com>
2023-05-17 15:57:55 -0300

    Update FDE-IEH.py

--- FDE-IEH.py
+++ FDE-IEH.py
@@ -31,11 +31,11 @@ print('============================================================')
 
 # crackmapexec
 print('''
-    ======================================================================================
-    Because crackmapexe is giving an error in the code, let's open it in another terminal
-    But once you discover all the hosts you can close the new terminal and continue
-    ======================================================================================
-    ''')
+======================================================================================
+Because crackmapexe is giving an error in the code, let's open it in another terminal
+But once you discover all the hosts you can close the new terminal and continue
+======================================================================================
+''')
 
 command = 'gnome-terminal -- bash -c "crackmapexec smb targets; exec bash"'
 
@@ -50,7 +50,7 @@ def nmserver():
     klm = input('Sever AD Recon - AD ip: ')
     os.system(f'nmap -v --open -Pn {klm}')
 nmserver()
-
+print('============================================================')
 continuar1 = input('Have More AD Hosts to scan[yes/no] ')
 if continuar1.lower == "yes":
         nmserver()
@@ -63,10 +63,12 @@ port53 = input('Any 53 port in Ad host? [yes/no] ')
 if port53 == "yes":    
     # Resolucao de DNS
     def dnsresol():
+        print('============================================================')
         print('DNS Resolution')
         first_host = input('Server AD Host: ')
         second_host = input('Second Host: ')
         os.system(f'host {first_host} {second_host}')
+        print('============================================================')
     dnsresol()
 
     # host
